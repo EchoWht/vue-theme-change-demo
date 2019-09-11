06dd2f78cc53909483503f63d200d6c79ae650cf
blskye <echowht@qq.com>
2019-09-11 12:02:54 +0800

    更改为更容易理解的方式

--- src/components/TestVuex/store.js
+++ src/components/TestVuex/store.js
@@ -6,8 +6,12 @@ const store = new Vuex.Store({
     count: 0
   },
   mutations: {
-    increment: state => state.count++,
-    decrement: state => state.count--
+    increment(state){
+      state.count++
+    },
+    decrement(state){
+      state.count--
+    },
   }
 })
 export default store
